912cbe1a0d3f96e80e8c2c506bcb05fcc880f1ef
10askinsw <10askinsw@gmail.com>
2015-08-16 20:47:22 +0100

    ArrowCamera distance doubled

--- net/yawk/client/cameras/ArrowCamera.java
+++ net/yawk/client/cameras/ArrowCamera.java
@@ -44,7 +44,7 @@ public class ArrowCamera extends Camera{
 				&& !currentArrow.inGround
 				&& !currentArrow.isCollided
 				&& !currentArrow.isDead
-				&& currentArrow.getDistanceToEntity(mc.thePlayer) < 500
+				&& currentArrow.getDistanceToEntity(mc.thePlayer) < 1000
 				&& mc.theWorld.loadedEntityList.contains(currentArrow);
 	}
 }
