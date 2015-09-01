9e6ab9ac00252472316e8fe53f3a33ba73a824f6
feSLsx <LukasJezMusic@gmail.com>
2015-09-01 03:28:33 -0800

    Added Help Command + New Color thing
    Added Help Command, and a new color thing cuz the other one is bad.

---
+++ net/yawk/client/command/commands/Help.java
@@ -0,0 +1,23 @@
+package net.yawk.client.command.commands;
+
+import net.yawk.client.Client;
+import net.yawk.client.command.Command;
+import net.yawk.client.utils.ChatColor;
+import net.yawk.client.utils.Colours;
+
+public class Help extends Command {
+	
+	public Help() {
+		super("Help", "help", "Shows you commands.");
+	}
+	
+	@Override
+	public void runCommand(String message) {
+		for(Command c : Client.getClient().getCommandManager().getCommands()) {
+			if(c instanceof Help) {
+				continue;
+			}
+			Client.getClient().addChat(ChatColor.RED + c.getName() + ChatColor.GRAY + " | " + ChatColor.GOLD + "." + c.getCallName() + ChatColor.GRAY + " | " + ChatColor.DARK_GRAY + c.getDesc());
+		}
+	}
+}
