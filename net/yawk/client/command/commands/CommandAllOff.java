25862f93b59db3948bd8de4d758aa353f4fdb8d6
10askinsw <10askinsw@gmail.com>
2015-09-02 09:25:27 +0100

    CommandAllOff added

---
+++ net/yawk/client/command/commands/CommandAllOff.java
@@ -0,0 +1,32 @@
+package net.yawk.client.command.commands;
+
+import net.yawk.client.Client;
+import net.yawk.client.command.Argument;
+import net.yawk.client.command.Arguments;
+import net.yawk.client.command.Command;
+import net.yawk.client.command.CommandManager;
+import net.yawk.client.modmanager.Mod;
+import net.yawk.client.utils.ChatColor;
+import net.yawk.client.utils.Colours;
+
+public class CommandAllOff extends Command {
+	
+	public CommandAllOff() {
+		super("AllOff", "alloff", "Switches all mods off");
+	}
+	
+	@Override
+	public void runCommand(CommandManager cm, Arguments args) {
+		
+		for(Mod m : Client.getClient().getModManager().mods){
+			if(m.isEnabled()){
+				Client.getClient().getModManager().toggle(m);
+			}
+		}
+	}
+
+	@Override
+	public Argument[] getArguments(CommandManager cm) {
+		return null;
+	}
+}
