fef8d2b566fa1521ffeffc01480688e7eefe3741
10askinsw <10askinsw@gmail.com>
2015-09-03 19:19:23 +0100

    Perception hack added

---
+++ net/yawk/client/mods/world/Perception.java
@@ -0,0 +1,29 @@
+package net.yawk.client.mods.world;
+
+import net.minecraft.client.entity.EntityOtherPlayerMP;
+import net.minecraft.entity.Entity;
+import net.yawk.client.Client;
+import net.yawk.client.events.EventTick;
+import net.yawk.client.modmanager.Mod;
+import net.yawk.client.modmanager.RegisterMod;
+
+import com.darkmagician6.eventapi.EventTarget;
+
+@RegisterMod(name = "Perception", desc = "See when people spawn near you", type = Mod.Type.WORLD)
+public class Perception extends Mod{
+	
+	public Perception(){
+		
+	}
+	
+	private static String lastPlayer;
+	
+	//Called by RenderGlobalHook
+	public void onEntityAdded(Entity e){
+		
+		if(isEnabled() && e instanceof EntityOtherPlayerMP && Client.getClient().getPlayer().ticksExisted > 5 && lastPlayer != e.getName()){
+			Client.getClient().addChat("Player spawned nearby: "+e.getName());
+			lastPlayer = e.getName();
+		}
+	}
+}
