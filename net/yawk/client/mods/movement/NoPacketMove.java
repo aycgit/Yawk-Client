1cac3611a962a8e16026c4fe44244244780c6ef9
10askinsw <10askinsw@gmail.com>
2015-09-04 21:51:22 +0100

    NoPacketMove added

---
+++ net/yawk/client/mods/movement/NoPacketMove.java
@@ -0,0 +1,33 @@
+package net.yawk.client.mods.movement;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.play.client.C03PacketPlayer;
+import net.yawk.client.Client;
+import net.yawk.client.events.EventSendPacket;
+import net.yawk.client.events.EventTick;
+import net.yawk.client.modmanager.Mod;
+import net.yawk.client.modmanager.RegisterMod;
+
+import com.darkmagician6.eventapi.EventTarget;
+
+@RegisterMod(name = "NoPacketMove", desc = "Don't send packets when not moving", type = Mod.Type.MOVEMENT)
+public class NoPacketMove extends Mod{
+	
+	private Minecraft mc;
+	
+	public NoPacketMove(){
+		mc = Minecraft.getMinecraft();
+	}
+	
+	@EventTarget
+	public void onSend(EventSendPacket e){
+		if(e.packet instanceof C03PacketPlayer){
+			e.setCancelled(!isMoving());
+		}
+	}
+	
+	private boolean isMoving(){
+		return mc.gameSettings.keyBindForward.getIsKeyPressed() || mc.gameSettings.keyBindLeft.getIsKeyPressed() || mc.gameSettings.keyBindRight.getIsKeyPressed() ||  mc.gameSettings.keyBindBack.getIsKeyPressed() || mc.gameSettings.keyBindJump.getIsKeyPressed() || mc.gameSettings.keyBindSneak.getIsKeyPressed();
+	}
+
+}
